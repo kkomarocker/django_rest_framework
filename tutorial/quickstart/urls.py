ce919d44d5265d0e464f182eedc5c71db18456d4
Jay Lee <jlee@els.edu>
2020-01-10 16:15:40 -0500

    removed quickstart

--- tutorial/quickstart/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .views import UserViewSet, GroupViewSet
-
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'groups', GroupViewSet)
-
-urlpatterns = router.urls
